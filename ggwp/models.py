fa77d1b21444a9d22f810c2c8a42afe835507faa
SamiulKabir <samkabir26@gmail.com>
2022-02-08 18:21:58 +0600

    askdjasd

--- ggwp/models.py
+++ ggwp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class actor(models.Model):
+      name = models.CharField(max_length=30, null=True, blank=True,)
+      age = models.IntegerField(max_length=40, null=True, blank=True )
+      moveis = models.OneToManyField()
+
+
+class movie(models.Model):
+      name = models.CharField(max_length=30, null=True, blank=True,)
+      age = models.IntegerField(max_length=40, null=True, blank=True )
